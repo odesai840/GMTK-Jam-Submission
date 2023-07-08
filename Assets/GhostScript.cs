749662eb226c7373c9c02ea658870757d504f96a
Philip Smith <philip@randomsmiths.com>
2023-07-07 22:36:34 -0400

    add ghost and glow

---
+++ Assets/GhostScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostScript : MonoBehaviour
+{
+
+    bool enabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enabled){
+            if(Input.GetKey("w")){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(velo.x, 3, 0);
+            } else if(Input.GetKey("s")){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(velo.x, -3, 0);
+            } else {
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(velo.x, 0, 0);
+            }
+    
+            if(Input.GetKey("a")){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(-3, velo.y, 0);
+            } else if(Input.GetKey("d")){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(3, velo.y, 0);
+            } else {
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, velo.y, 0);
+            }
+        }
+    }
+}
---
+++ Assets/HunterScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterMovement : MonoBehaviour
+{
+    bool wDown = false;
+    bool enabled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enabled){
+            if(Input.GetKey("w") && !wDown){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(velo.x, 5, 0);
+            }
+            wDown = Input.GetKey("w");
+
+            if(Input.GetKey("a")){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(-3, velo.y, 0);
+            } 
+
+            if(Input.GetKey("d")){
+                Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(3, velo.y, 0);
+            }
+        }
+        
+
+    }
+}
