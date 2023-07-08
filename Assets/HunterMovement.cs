e6cd9dd55d64cfa60e11fbb4287d4a24d9f2d862
Philip Smith <philip@randomsmiths.com>
2023-07-07 21:30:02 -0400

    add movement to the sprite the same way the hunters move

--- Assets/HunterMovement.cs
+++ Assets/HunterMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class HunterMovement : MonoBehaviour
 {
+    bool wDown = false;
+    bool sDown = false;
+    bool enabled = false;
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey("w") && !wDown){
+            Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+            GetComponent<Rigidbody2D>().velocity = new Vector3(velo.x, 5, 0);
+        }
+        wDown = Input.GetKey("w");
+
+        if(Input.GetKey("a")){
+            Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+            GetComponent<Rigidbody2D>().velocity = new Vector3(-3, velo.y, 0);
+        } 
+
+
+        if(Input.GetKey("d")){
+            Vector3 velo = GetComponent<Rigidbody2D>().velocity;
+            GetComponent<Rigidbody2D>().velocity = new Vector3(3, velo.y, 0);
+        }
+
     }
 }
