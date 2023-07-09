600dc564c03ee3f3798955cdc6453fe863ecf2b7
Philip Smith <philip@randomsmiths.com>
2023-07-08 23:23:01 -0400

    ghosts meander

--- Assets/GhostScript.cs
+++ Assets/GhostScript.cs
@@ -10,6 +10,7 @@ public class GhostScript : CharacterBase
     SpriteRenderer m_SpriteRenderer;
     //The Color to be assigned to the Renderer’s Material
     float speed = 5;
+    int frameCount = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -51,16 +52,33 @@ public class GhostScript : CharacterBase
                 Vector3 velo = GetComponent<Rigidbody2D>().velocity;
                 GetComponent<Rigidbody2D>().velocity = new Vector3(0, velo.y, 0);
             }
-        } else if (level<=10){
-            m_SpriteRenderer.color = Color.yellow;
-        } else if (level>10 && level<=20){
-            m_SpriteRenderer.color = Color.blue;
-        } else if (level>20 && level<=30){
-            m_SpriteRenderer.color = Color.black;
-        } else{
-            m_SpriteRenderer.color = Color.white;
-        }            
-         base.Update();
+        } else {
+            if (level<=10){
+                m_SpriteRenderer.color = Color.yellow;
+            } else if (level>10 && level<=20){
+                m_SpriteRenderer.color = Color.blue;
+            } else if (level>20 && level<=30){
+                m_SpriteRenderer.color = Color.black;
+            } else{
+                m_SpriteRenderer.color = Color.white;
+            }
+
+
+        }
+        base.Update();
+        
+    }
+
+    void FixedUpdate(){
+
+        if(!controlled){
+            if(frameCount <= 0){
+                GetComponent<Rigidbody2D>().velocity = new Vector3(Random.Range(-.75f, .75f)*speed, Random.Range(-.75f, .75f)*speed, 0);
+                frameCount = 50;
+            }
+
+            frameCount--;
+        }
         
     }
 }
