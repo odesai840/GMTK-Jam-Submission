b5e93ad53c828136f9f6ae0b3d482bf34fcf9611
vrayapal <125930690+vrayapal@users.noreply.github.com>
2023-07-09 10:14:23 -0400

    the camera never lies
    the camera no longer clips out of the map and continues to folow players until their unforeseen demise. who could have seen this coming?

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,6 +7,12 @@ public class CameraFollow : MonoBehaviour
 
     public Transform followTransform;
     // Start is called before the first frame update
+    private float ylowerlimit =  6.2f;
+    private float yupperlimit =  70.7f;
+    private float xlowerlimit =  -62.6f;
+    private float xupperlimit =  41.62f;
+    private float x = 0;
+    private float y = 0;
     void Start()
     {
         
@@ -15,6 +21,20 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        if(followTransform.position.x <= xlowerlimit) {
+            x=xlowerlimit;
+        } else if(followTransform.position.x >= xupperlimit) {
+            x=xupperlimit;
+        } else {
+            x= followTransform.position.x;
+        }
+        if(followTransform.position.y <= ylowerlimit) {
+            y=ylowerlimit;
+        } else if(followTransform.position.y >= yupperlimit) {
+            y=yupperlimit;
+        } else {
+            y= followTransform.position.y;
+        }
+        this.transform.position = new Vector3(x, y, this.transform.position.z);
     }
 }
